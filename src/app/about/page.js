6ddaf593554891b5121bf8abb025e56340d2ba44
neha-sublime <kashishjivani2002@gmail.com>
2023-09-20 16:31:37 +0530

    changes in logo

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,7 @@ import React from "react";
 const About = () => {
     return (
         <>
-            <Herosection title={"About Page"} imageUrl={"/about1.svg"}/>
+            <Herosection title={"OUR STORY"} imageUrl={"/about1.svg"}/>
         </>
     );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <header className={styles.main_header}>
             <div className={styles.navbar_brand}>
                 <Link href="/" >
-                    <Image src="/logo.png" alt="my logo" width={150} height={40}/>
+                    <Image src="/moviesverseLogo.png" alt="my logo" width={250} height={150}/>
                 </Link>
             </div>
             <Nav />
